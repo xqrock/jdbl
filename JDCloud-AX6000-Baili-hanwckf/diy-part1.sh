1235014d345d68e35b0118d7f8372f695a77b94c
xqrock <58764635+xqrock@users.noreply.github.com>
2026-03-07 23:58:47 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -42,9 +42,7 @@ mkdir -p package/diy
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/diy/passwall-packages
-rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/diy/passwall-luci
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/diy/passwall2
 # git clone https://github.com/gdy666/luci-app-lucky.git package/diy/lucky
