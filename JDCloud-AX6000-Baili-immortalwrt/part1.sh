2e1b0a8817646dc0a3557d2a2a863609f2a26ac2
xqrock <58764635+xqrock@users.noreply.github.com>
2026-03-27 01:08:54 +0800

    Update part1.sh

--- JDCloud-AX6000-Baili-immortalwrt/part1.sh
+++ JDCloud-AX6000-Baili-immortalwrt/part1.sh
@@ -46,9 +46,5 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 extract_pkg "https://github.com/kenzok8/openwrt-packages" "adguardhome" "package/adguardhome"
 extract_pkg "https://github.com/vernesong/OpenClash" "luci-app-openclash" "package/luci-app-openclash"
-git clone --depth=1 https://github.com/padavanonly/immortalwrt-mt798x-6.6 temp_repo
-cp -rn temp_repo/package/mtk/applications/wrtbwmon package/
-cp -rn temp_repo/package/mtk/applications/luci-app-wrtbwmon package/
-rm -rf feeds/luci/applications/luci-app-wrtbwmon
-rm -rf feeds/packages/net/wrtbwmon
-rm -rf temp_repo
+extract_pkg "https://github.com/timsaya/luci-app-bandix" "luci-app-bandix" "package/luci-app-bandix"
+extract_pkg "https://github.com/timsaya/openwrt-bandix" "openwrt-bandix" "package/openwrt-bandix"
