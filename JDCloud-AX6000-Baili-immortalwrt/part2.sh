8b2c74a77cbfe0655962644f4aab733c3779de9c
xqrock <58764635+xqrock@users.noreply.github.com>
2026-04-14 14:48:02 +0800

    Update part2.sh

--- JDCloud-AX6000-Baili-immortalwrt/part2.sh
+++ JDCloud-AX6000-Baili-immortalwrt/part2.sh
@@ -24,6 +24,8 @@ rm -rf feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/shadowsocks-libev
 # rm -rf ./feeds/packages/net/shadowsocks-rust
 # rm -rf ./feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 #修复Rust编译失败
